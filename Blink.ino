b48650d8d4174362d0e40bc2799f2e56477847f8
ozgurkargin <81071695+ozgurkargin@users.noreply.github.com>
2021-03-21 11:12:32 +0300

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,9 +1,9 @@
 
-  // initialize digital pin LED_BUILTIN as an output.
+  
   pinMode(8, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
   digitalWrite(8, HIGH);  
   delay(1000);                       
